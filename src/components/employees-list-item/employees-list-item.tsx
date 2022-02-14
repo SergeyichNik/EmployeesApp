a4e29cb9cc1ffd6940d9b65a054f27de645d1f27
SergeichNik <tyunev.nik89@gmail.com>
2022-02-14 15:53:06 +0300

    props and some logic

--- src/components/employees-list-item/employees-list-item.tsx
+++ src/components/employees-list-item/employees-list-item.tsx
@@ -1,10 +1,23 @@
 import './employees-list-item.css';
 
-const EmployeesListItem = () => {
+type PropsType = {
+    name: string,
+    salary: number,
+    increase: boolean
+}
+
+const EmployeesListItem = (props: PropsType) => {
+    const {name, salary, increase} = props
+
+    let increaseClass = "list-group-item d-flex justify-content-between"
+    if(increase) {
+        increaseClass = "list-group-item d-flex justify-content-between increase"
+    }
+
     return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="2000$"/>
+        <li className={increaseClass}>
+            <span className="list-group-item-label">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + ' $'}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm ">
--- src/components/employees-list/employees-list.tsx
+++ src/components/employees-list/employees-list.tsx
@@ -1,13 +1,23 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
+type DataType = {
+    name: string,
+    salary: number,
+    increase: boolean
+}
+type PropsType = {
+    data: DataType[]
+}
 
-const EmployeesList = () => {
+const EmployeesList = (props: PropsType) => {
+    const {data} = props
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {data.map((item) => {
+                return <EmployeesListItem name={item.name} salary={item.salary} increase={item.increase}/>})
+            }
+
         </ul>
     )
 }
